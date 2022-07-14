f42ecb3a8d39d07222e9e47b9b42f64a455d575b
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-14 15:04:09 +0300

    Fetching and displaying the data

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
-import { dehydrate, QueryClient, useQuery } from 'react-query';
-import axios from 'axios';
-
+import axios from "axios";
+import { useState } from "react";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+import DisplayDrink from "../components/DisplayDrink";
 
 export const getServerSideProps = async (context) => {
-  const queryClient = new QueryClient()
- let data;
-  const getCocktails=() => axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=').then((response) => data = response.data)
-  await queryClient.prefetchQuery('cocktails',getCocktails)
- //await axios.get('www.thecocktaildb.com/api/json/v1/1/search.php?f=a').then((response) => cocktails = response.data)
- 
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["cocktails"], () =>
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+      .then((response) => (data = response.data))
+  );
+
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
-  }
-}
-
+  };
+};
 
 export default function Home() {
-  let data;
-  const getCocktails=() => axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=').then((response) => data = response.data)
-  const {data: mn} = useQuery('cocktails', getCocktails)
-  console.log(mn)
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [search, setSearch] = useState("");
+  const { isLoading, isSuccess, isError, data, error } = useQuery(
+    ["cocktails", search],
+    () =>
+      axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
+      )
+  );
+  return (
+    <div>
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-10 p-6">
+      {data?.data?.drinks.map((drink, id) => (
+        <DisplayDrink key={id} cocktail={drink} />
+      ))}
+    </div>
+    </div>
+  );
 }
