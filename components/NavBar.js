5d928b7e4b3b1e33e11eba5a4f846970581e13b5
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-18 13:37:07 +0300

    Adding a dark theme to the website

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 
 const NavBar = () => {
+  const [mounted, setMounted] = useState(false);
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  const renderTheme = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "dark") {
+      return (
+        <SunIcon className="w-6 h-6" onClick={() => setTheme("light")} />
+      );
+    } else {
+      return (
+        <MoonIcon className="w-6 h-6" onClick={() => setTheme("dark")} />
+      );
+    }
+  };
   return (
     <div className="flex sticky flex-row z-20 bg-white/30 justify-between items-center w-full py-4 px-8 top-0 backdrop-blur-md">
       <h1 className="font-bold text-xl">Cocktails</h1>
       <div className="flex flex-row justify-between w-1/6 mr-6">
+        {renderTheme()}
         <h1 className="font-semibold">Home</h1>
         <h1 className="font-semibold">About</h1>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,19 @@ import { useState } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools'
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   const[queryClient] = useState(()=> new QueryClient)
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.deHydratedState}>
+        <ThemeProvider enableSystem={true} attribute="class">
         <NavBar/>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
         <Footer/>
+        </ThemeProvider>
       </Hydrate>
     </QueryClientProvider>
   );
