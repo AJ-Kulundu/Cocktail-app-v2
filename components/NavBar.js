885f5d3e74b60c7806f0339f21587972264262ae
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-18 13:03:12 +0300

    Adding a backdrop blur to the nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row justify-between w-full py-4 px-8">
+    <div className="flex sticky flex-row z-20 bg-white/30 justify-between items-center w-full py-4 px-8 top-0 backdrop-blur-md">
       <h1 className="font-bold text-xl">Cocktails</h1>
       <div className="flex flex-row justify-between w-1/6 mr-6">
         <h1 className="font-semibold">Home</h1>
