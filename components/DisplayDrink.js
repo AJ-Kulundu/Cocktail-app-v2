01bfe07b9dee1679bdad57ae126a19b622b62add
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-21 12:16:59 +0300

    Adding links to the Display drink component

--- components/DisplayDrink.js
+++ components/DisplayDrink.js
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-function booleanFilter (...classes) {
-    return classes.filter(Boolean).join(" ")
+function booleanFilter(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
+const DisplayDrink = ({ cocktail }) => {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
-const DisplayDrink = ({cocktail}) => {
-    const [loading,setLoading] = useState(true);
-  
-    return(
-        <div className='flex flex-col rounded-lg px-6 py-4 gap-y-2 shadow-md'>
+  return (
+    <div className="group">
+      <div className="flex flex-col rounded-lg px-6 py-4 gap-y-2">
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg">
-        <Image
-          alt=" "
-          src={!cocktail.strDrinkThumb ? "https://bit.ly/placeholder-img" : cocktail.strDrinkThumb}
-          layout="fill"
-          objectFit="cover"
-          className={booleanFilter(
-            "duration-700 ease-in-out group-hover:opacity-75",
-            loading
-              ? "scale-110 blur-2xl grayscale"
-              : "scale-100 blur-0 grayscale-0"
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
+          <Image
+            alt={cocktail.strDrink}
+            src={
+              !cocktail.strDrinkThumb
+                ? "https://bit.ly/placeholder-img"
+                : cocktail.strDrinkThumb
+            }
+            layout="fill"
+            objectFit="cover"
+            className={booleanFilter(
+              "duration-700 ease-in-out group-hover:opacity-75",
+              loading
+                ? "scale-110 blur-2xl grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            )}
+            onLoadingComplete={() => setLoading(false)}
+            onClick={() => router.push(`/${cocktail.idDrink}`)}
+          />
         </div>
-        <h1>{cocktail.strDrink}</h1>
-        <h3>{cocktail.strCategory}</h3>
-        </div>
-    )
-}
+        <h1
+          className="text-md hover:underline font-medium group-hover:tracking-wide group-hover:font-bold"
+          onClick={() => router.push(`/${cocktail.idDrink}`)}
+        >
+          {cocktail.strDrink}
+        </h1>
+        <h3 className="text-lg font-semibold group-hover:tracking-wide">{cocktail.strCategory}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default DisplayDrink
+export default DisplayDrink;
