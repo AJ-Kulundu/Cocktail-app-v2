8cbdf1ffa7891311b8742912fb87b3774db4a61d
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-26 19:41:57 +0300

    Adding a margin to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex w-full p-8 justify-center bottom-0">
+    <div className="flex w-full p-8 justify-center inset-x-0 bottom-0 mt-5">
       <h1 className="font-semibold">Desgned + Coded by AJ Kulundu</h1>
     </div>
   );
