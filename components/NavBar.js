ba7a7e49ccadf59de44cadb3aa429f48b9ab9a4f
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-27 20:21:20 +0300

    Making the navbar responsive

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import { MoonIcon, SunIcon } from "@heroicons/react/solid";
+import { MoonIcon, SunIcon, XIcon, MenuIcon } from "@heroicons/react/solid";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
 
@@ -14,23 +15,39 @@ const NavBar = () => {
     const currentTheme = theme === "system" ? systemTheme : theme;
 
     if (currentTheme === "dark") {
-      return (
-        <SunIcon className="w-6 h-6" onClick={() => setTheme("light")} />
-      );
+      return <SunIcon className="w-6 h-6" onClick={() => setTheme("light")} />;
     } else {
-      return (
-        <MoonIcon className="w-6 h-6" onClick={() => setTheme("dark")} />
-      );
+      return <MoonIcon className="w-6 h-6" onClick={() => setTheme("dark")} />;
+    }
+  };
+
+  const menuToggle = () => {
+    if (isOpen) {
+      return <XIcon className="w-6 h-6" onClick={() => setIsOpen(false)} />;
+    } else {
+      return <MenuIcon className="w-6 h-6" onClick={() => setIsOpen(true)} />;
     }
   };
   return (
-    <div className="flex sticky flex-row z-20 bg-white/30 justify-between items-center w-full py-4 px-8 top-0 shadow-sm backdrop-blur-md">
+    <div className="flex sticky flex-col z-20 bg-white/30 top-0 shadow-sm backdrop-blur-md">
+      <div className="flex flex-row justify-between items-center w-full py-4 px-8">
       <h1 className="font-bold text-xl">Cocktails</h1>
-      <div className="flex flex-row justify-between w-1/6 mr-6">
+      <div className="hidden md:flex flex-row justify-between w-1/6 mr-6">
         {renderTheme()}
         <h1 className="font-semibold">Home</h1>
         <h1 className="font-semibold">About</h1>
       </div>
+      <div className="flex md:hidden flex-row justify-between w-1/5 ">
+        {renderTheme()}
+        {menuToggle()}
+      </div>
+      </div>
+      {isOpen && (
+        <div className="relative flex flex-col sm:display-inline md:hidden w-full p-4 gap-y-2 justify-center items-center">
+          <h1 className="font-semibold">Home</h1>
+          <h1 className="font-semibold">About</h1>
+        </div>
+      )}
     </div>
   );
 };
